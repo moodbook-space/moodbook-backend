9b87b0b85ec81df0175503f7274064f546378212
codesche <codesche@gmail.com>
2025-07-09 11:18:37 +0900

    feat(entity): User 엔티티 작성

---
+++ src/main/java/org/com/moodbook/user/entity/User.java
@@ -0,0 +1,55 @@
+package org.com.moodbook.user.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.com.moodbook.common.constants.Role;
+import org.com.moodbook.common.constants.UserStatus;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UserProfile userProfile;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String contact;
+
+    @Column(nullable = false)
+    private boolean emailVerified;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+}
