bf8588b2c119d48cca4d65a406aa9c195bcbe368
codesche <codesche@gmail.com>
2025-07-09 11:19:40 +0900

    feat(entity): 생성 시간, 수정 시간 변수 공통 적용

--- src/main/java/org/com/moodbook/MoodbookApplication.java
+++ src/main/java/org/com/moodbook/MoodbookApplication.java
@@ -2,8 +2,10 @@ package org.com.moodbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MoodbookApplication {
     // dev 브랜치 생성
     public static void main(String[] args) {
