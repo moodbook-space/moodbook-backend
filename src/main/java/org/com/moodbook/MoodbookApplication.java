a1ac507da0a4c88d06d60d647454c811b5fb6766
codesche <codesche@gmail.com>
2025-07-09 10:04:24 +0900

    Project Setting

---
+++ src/main/java/org/com/moodbook/MoodbookApplication.java
@@ -0,0 +1,13 @@
+package org.com.moodbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodbookApplication.class, args);
+    }
+
+}
