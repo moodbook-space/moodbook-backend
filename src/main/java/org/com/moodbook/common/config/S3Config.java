81286ef7a9690521ac484d302c37aabc51078bac
LteFroggy <zac0328@naver.com>
2025-07-10 16:39:59 +0900

    feat(Config) : S3Client Bean을 포함한 S3Config 파일 생성(#13)

---
+++ src/main/java/org/com/moodbook/common/config/S3Config.java
@@ -0,0 +1,29 @@
+package org.com.moodbook.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+  @Value("${cloud.aws.credentials.access-key}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secret-key}")
+  private String secretKey;
+
+  @Value("${cloud.aws.region.static}")
+  private String region;
+
+  @Bean
+  public S3Client s3Client() {
+    return S3Client.builder()
+        .region(Region.AP_NORTHEAST_2) // 서울
+        .credentialsProvider(ProfileCredentialsProvider.create()) // or env, static
+        .build();
+  }
+}
