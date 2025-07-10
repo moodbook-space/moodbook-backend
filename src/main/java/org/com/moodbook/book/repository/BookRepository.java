49700ee3ed3edbd1435e6ea2a2144dabd9540e4d
sagecode <iamsage9346@gmail.com>
2025-07-10 17:46:14 +0900

    feat/#23:BookRepository 생성

---
+++ src/main/java/org/com/moodbook/book/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package org.com.moodbook.book.repository;
+
+import org.com.moodbook.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, String> {
+
+}
