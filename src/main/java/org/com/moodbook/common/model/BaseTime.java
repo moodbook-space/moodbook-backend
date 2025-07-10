ad8a11ec184695edbc9b86e25d4a4d5f8ae9a8b2
kimdonghyeon <dhb6306@gmail.com>
2025-07-10 21:14:14 +0900

    config(BaseTime)-model 패키지 생성 및 필요 어노테이션 추가

--- src/main/java/org/com/moodbook/common/model/BaseTime.java
+++ src/main/java/org/com/moodbook/common/model/BaseTime.java
@@ -1,9 +1,11 @@
-package org.com.moodbook.user.entity;
+package org.com.moodbook.common.model;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +13,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
 public abstract class BaseTime {        // 생성일자, 수정일자 공통 적용
 
     @CreatedDate
