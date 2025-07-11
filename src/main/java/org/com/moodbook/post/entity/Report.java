aca349703658ee5bb962ee2456e5cf2843a827af
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-11 17:58:29 +0900

    feat(Entity): 독서모임 엔티티 코드 작성(#41)

---
+++ src/main/java/org/com/moodbook/post/entity/Report.java
@@ -0,0 +1,31 @@
+package org.com.moodbook.post.entity;
+
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.com.moodbook.book.entity.Book;
+
+
+@Entity
+@DiscriminatorValue("REPORT")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Report extends BasePost {
+
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "book_id", nullable = false)
+  private Book book;
+}
+
