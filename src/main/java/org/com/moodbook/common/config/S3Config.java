07823c6f2e3edc8538d5f89d0fc0ba47bea06ed5
LteFroggy <zac0328@naver.com>
2025-07-11 17:26:55 +0900

    feat(Config) : S3Config 파일에 주석 추가

--- src/main/java/org/com/moodbook/common/config/S3Config.java
+++ src/main/java/org/com/moodbook/common/config/S3Config.java
@@ -3,7 +3,9 @@ package org.com.moodbook.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -21,9 +23,15 @@ public class S3Config {
 
   @Bean
   public S3Client s3Client() {
+    // application.yml 또는 properties에서 주입받은 accessKey, secretKey로 AWS 자격 증명 객체 생성
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
     return S3Client.builder()
-        .region(Region.AP_NORTHEAST_2) // 서울
-        .credentialsProvider(ProfileCredentialsProvider.create()) // or env, static
+        // AWS 리전 설정 (예: ap-northeast-2)
+        .region(Region.of(region))
+        // 정적 자격 증명 제공자에 위에서 만든 credentials를 설정
+        .credentialsProvider(StaticCredentialsProvider.create(credentials))
+        // 최종적으로 S3Client 객체 생성
         .build();
   }
 }
