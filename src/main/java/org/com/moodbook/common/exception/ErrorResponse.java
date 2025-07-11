ec91cee6c86fde3051622d34be23541488021882
codesche <codesche@gmail.com>
2025-07-11 10:55:51 +0900

    feat(exception): 커스텀 예외 처리 - ErrorResponse (#30)

---
+++ src/main/java/org/com/moodbook/common/exception/ErrorResponse.java
@@ -0,0 +1,26 @@
+package org.com.moodbook.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String code;            // 상태코드
+    private String message;         // 커스텀 에러 메세지
+
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+            .code(errorCode.getCode())
+            .message(errorCode.getMessage())
+            .build();
+    }
+
+    public void changeMessage(String message) {
+        this.message = message;
+    }
+
+}
