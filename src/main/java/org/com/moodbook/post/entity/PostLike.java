1841e2b8684b01272c6b07e0b2cd45b3fe9b9a96
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-12 01:17:33 +0900

    feat(Entity): 좋아요 엔티티 코드 작성(#48) - PostLike

---
+++ src/main/java/org/com/moodbook/post/entity/PostLike.java
@@ -0,0 +1,45 @@
+package org.com.moodbook.post.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.com.moodbook.common.model.BaseTime;
+import org.com.moodbook.member.entity.Member;
+
+/**
+ * 게시글 좋아요(PostLike) 엔티티
+ */
+@Entity
+@Table(
+    name = "post_like",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uk_post_member",
+        columnNames = {"post_id", "member_id"}
+    )
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class PostLike extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  /**
+   * 좋아요가 달린 게시글
+   */
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private BasePost post;
+
+  /**
+   * 좋아요를 누른 회원
+   */
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+}
