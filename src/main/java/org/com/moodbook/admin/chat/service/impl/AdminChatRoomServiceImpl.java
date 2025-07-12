ec7a4fd4c4c940985a9fedca9c8d151cdb87f7e9
roode <roode1017@gmail.com>
2025-07-12 12:22:58 +0900

    feat(service) : 관리자 채팅방 관리용 service 구현체 추가(#42)

---
+++ src/main/java/org/com/moodbook/admin/chat/service/impl/AdminChatRoomServiceImpl.java
@@ -0,0 +1,44 @@
+package org.com.moodbook.admin.chat.service.impl;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.com.moodbook.admin.chat.dto.AdminChatRoomDTO;
+import org.com.moodbook.admin.chat.repository.AdminChatRoomRepository;
+import org.com.moodbook.admin.chat.service.AdminChatRoomService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AdminChatRoomServiceImpl implements AdminChatRoomService {
+
+  private final AdminChatRoomRepository adminChatRepository;
+
+
+
+  //채팅방 리스트 페이지로 불러오기
+  @Override
+  public Page<AdminChatRoomDTO> getChatList(Pageable pageable) {
+    return adminChatRepository.findAllPaging(pageable);
+  }
+
+  //채팅방 삭제
+  public void deleteById(Long chatId){
+    if(!adminChatRepository.existsById(chatId)){
+      throw new NoSuchElementException("해당 ID의 채팅방이 존재하지 않습니다.");
+    }
+    adminChatRepository.deleteById(chatId);
+  }
+
+  //채팅방 검색
+  @Override
+  public Page<AdminChatRoomDTO> searchChats(String query, Pageable pageable) {
+    if (query == null || query.isBlank()) {
+      return adminChatRepository.findAllPaging(pageable); // 기존 전체 목록
+    }
+    return adminChatRepository.findByNameOrCreatedBy(query, pageable);
+  }
+
+}
