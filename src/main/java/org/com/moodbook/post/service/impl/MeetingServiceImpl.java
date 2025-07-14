3e8453b458f98f3272578dd4cc2f6a83bbb633b4
kimdonghyeon <dhb6306@gmail.com>
2025-07-14 15:39:44 +0900

    feat(service)- service에 user import부분 member로 변경

--- src/main/java/org/com/moodbook/post/service/impl/MeetingServiceImpl.java
+++ src/main/java/org/com/moodbook/post/service/impl/MeetingServiceImpl.java
@@ -39,7 +39,7 @@ public class MeetingServiceImpl implements MeetingService {
   public Long createMeeting(Long memberId, CreateMeetingRequest req) {
     // 호스트 검증
     Member host = memberRepository.findById(memberId)
-        .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+        .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND));
 
     // 태그 조회
     List<MoodTag> tags = tagRepository.findAllById(req.getTagIds());
