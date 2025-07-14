bbba0ae244e358d37e0efdede5975dbe9a044a51
kimdonghyeon <dhb6306@gmail.com>
2025-07-14 10:11:12 +0900

    feat(controller)-임시회원가입 진행

--- src/main/java/org/com/moodbook/member/controller/MemberController.java
+++ src/main/java/org/com/moodbook/member/controller/MemberController.java
@@ -1,9 +1,28 @@
 package org.com.moodbook.member.controller;
 
 import lombok.AllArgsConstructor;
+import org.com.moodbook.member.dto.MemberDTO;
+import org.com.moodbook.member.dto.MemberTempJoinDto;
+import org.com.moodbook.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
-
+@RestController
+@RequestMapping("/api/oauth")
 public class MemberController {
+  private final MemberService memberService;
+
+  /**
+   * [POST] 임시 회원가입
+   */
+  @PostMapping("/tempSignUp")
+  public ResponseEntity<MemberDTO>  tempSignUp(@RequestBody MemberTempJoinDto dto) {
+    MemberDTO result = memberService.tempjoin(dto);
+    return ResponseEntity.ok(result);
+  }
 
 }
