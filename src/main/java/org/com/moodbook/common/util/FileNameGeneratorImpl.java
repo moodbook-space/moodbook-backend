1fe6d300bf9e87305d55a4520f76f3345831de2b
LteFroggy <zac0328@naver.com>
2025-07-14 15:30:07 +0900

    feat(util) : UuidFileNameGenerator -> FileNameGeneratorImpl 파일명 수정, 불필요한 toString 제거

--- src/main/java/org/com/moodbook/common/util/FileNameGeneratorImpl.java
+++ src/main/java/org/com/moodbook/common/util/FileNameGeneratorImpl.java
@@ -4,11 +4,11 @@ import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UuidFileNameGenerator implements FileNameGenerator {
+public class FileNameGeneratorImpl implements FileNameGenerator {
 
   @Override
   public String generateFileName(String originalFilename) {
     String ext = originalFilename.substring(originalFilename.lastIndexOf("."));
-    return UUID.randomUUID().toString() + ext;
+    return UUID.randomUUID() + ext;
   }
 }
