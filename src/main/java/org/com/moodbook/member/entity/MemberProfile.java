e8b374cb11efd619ce47f0986235f407aefe86b9
LteFroggy <zac0328@naver.com>
2025-07-14 15:58:21 +0900

    feat(entity) : PR Comment 수정사항 적용 (myImage 기본값 설명하는 주석 추가)

--- src/main/java/org/com/moodbook/member/entity/MemberProfile.java
+++ src/main/java/org/com/moodbook/member/entity/MemberProfile.java
@@ -40,6 +40,8 @@ public class MemberProfile {
   private String address;
 
   @Column(nullable = false)
+  // Default Value로 설정된 링크는 S3버킷 내의 "기본 프로필 이미지"가 저장된 링크입니다.
+  // 모든 회원은 프로필사진이 없는 상태로 회원가입되기에, "기본 프로필 이미지"의 링크를 Default Value로 적용하였습니다.
   private String myImage = "https://moodbook-bucket.s3.ap-northeast-2.amazonaws.com/131a3a40-191c-4a7a-83ee-af254efaf0fb.png";
 
   @Column(length = 100)
