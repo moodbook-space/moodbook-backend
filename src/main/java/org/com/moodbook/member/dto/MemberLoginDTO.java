bfea033ee515cdfd2086d5194a79b85002dc3712
kimdonghyeon <dhb6306@gmail.com>
2025-07-14 15:40:54 +0900

    feat(DTO)- 로그인 request DTO 생성

---
+++ src/main/java/org/com/moodbook/member/dto/MemberLoginDTO.java
@@ -0,0 +1,16 @@
+package org.com.moodbook.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginDTO {
+  private String email;
+  private String password;
+
+}
