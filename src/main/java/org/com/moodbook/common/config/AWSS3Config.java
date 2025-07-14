b7b4dac70fb1cc4feb205458f24666d2839128c1
LteFroggy <zac0328@naver.com>
2025-07-14 15:23:26 +0900

    feat(config) : AWSS3로 파일명 변경

--- src/main/java/org/com/moodbook/common/config/AWSS3Config.java
+++ src/main/java/org/com/moodbook/common/config/AWSS3Config.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-public class S3Config {
+public class AWSS3Config {
 
   @Value("${cloud.aws.credentials.access-key}")
   private String accessKey;
