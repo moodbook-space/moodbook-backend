9cbced421aa02ecf25bedd93b67872038d7f743d
kimdonghyeon <dhb6306@gmail.com>
2025-07-14 10:08:05 +0900

    feat(security)-filterChain메서드 구현

--- src/main/java/org/com/moodbook/security/config/SecurityConfig.java
+++ src/main/java/org/com/moodbook/security/config/SecurityConfig.java
@@ -4,20 +4,35 @@ import lombok.RequiredArgsConstructor;
 import org.com.moodbook.security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable//설정 클래스 등록
+@Configuration//설정 클래스 등록
 @EnableWebSecurity//스프링 시큐리티 활성화
 @RequiredArgsConstructor//생성자 자동 생성
 public class SecurityConfig {
 
   private final JwtTokenFilter jwtTokenFilter;
 
+  //회원가입시에 비밀번호를 암호화해주는 메서드
   @Bean
   public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
 
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable()) // csrf 비활성화
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/oauth/tempSignUp").permitAll()
+            .anyRequest().authenticated()
+        )
+        .build();
+  }
 
 }
