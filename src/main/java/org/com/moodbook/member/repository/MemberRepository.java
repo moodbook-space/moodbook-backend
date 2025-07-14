631037a5f6cc71ee793e76adb391897c905cc81a
kimdonghyeon <dhb6306@gmail.com>
2025-07-14 15:41:17 +0900

    feat(repository)- 로그인,회원가입시 필요한 jpa메서드 생성

--- src/main/java/org/com/moodbook/member/repository/MemberRepository.java
+++ src/main/java/org/com/moodbook/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package org.com.moodbook.member.repository;
 
+import java.util.Optional;
 import org.com.moodbook.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  //메서드 네임 규칙에 따른 추상 메서드
+  boolean existsByEmail(String email);
+  boolean existsByContact(String contact);
+
+  //가입된 회원 중 로그인을 위한 이메일 대조
+  Optional<Member> findByEmail(String email);
 }
