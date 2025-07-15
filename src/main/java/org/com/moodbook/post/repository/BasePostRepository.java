09d6ca878795b7b2b029fc75e3f58ec8e8f83c97
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-15 16:11:56 +0900

    feat(Repository): BasePost의 Repository 코드 작성(#77)

---
+++ src/main/java/org/com/moodbook/post/repository/BasePostRepository.java
@@ -0,0 +1,9 @@
+package org.com.moodbook.post.repository;
+
+import org.com.moodbook.post.entity.BasePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasePostRepository extends JpaRepository<BasePost, Long> {
+}
