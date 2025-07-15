2b5983cdd7b24f456d9aeb55111948a1812b711f
codesche <codesche@gmail.com>
2025-07-15 10:31:17 +0900

    feat(entity): BookCount 엔티티 생성 (#72)

---
+++ src/main/java/org/com/moodbook/book/entity/BookCount.java
@@ -0,0 +1,44 @@
+package org.com.moodbook.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book_count")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class BookCount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /** 도서와 1:1 관계 **/
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false, unique = true)
+    private Book book;
+
+    /** 조회 수 **/
+    @Column(name = "view_count", nullable = false)
+    private Long viewCount = 0L;
+
+    /** 조회수 증가 메서드 **/
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+
+}
