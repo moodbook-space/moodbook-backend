55255c6c14556fbae490f2e4a7b2e17df0d55051
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-15 19:17:41 +0900

    feat(Dto): 감정태그 목록 조회를 위한 Dto 코드 작성(#81) -TagResponse

---
+++ src/main/java/org/com/moodbook/post/dto/TagResponse.java
@@ -0,0 +1,12 @@
+package org.com.moodbook.post.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TagResponse {
+  private Long id;
+  private String name;
+}
