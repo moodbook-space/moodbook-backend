d9424f38156535c94fbffdc0adcebec126cfb731
sagecode <iamsage9346@gmail.com>
2025-07-15 15:31:34 +0900

    chore/#59: BookChat repository

---
+++ src/main/java/org/com/moodbook/bookchat/repository/ChatRoomMemberRepository.java
@@ -0,0 +1,33 @@
+package org.com.moodbook.bookchat.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.com.moodbook.bookchat.entity.ChatRoom;
+import org.com.moodbook.bookchat.entity.ChatRoomMember;
+import org.com.moodbook.bookchat.entity.ChatRoomMemberStatus;
+import org.com.moodbook.bookchat.entity.ChatRoomStatus;
+import org.com.moodbook.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
+
+  Boolean existsByChatRoomAndMember(ChatRoom chatRoom, Member member);
+  @Query("""
+    SELECT m FROM ChatRoomMember m
+    JOIN FETCH m.member
+    WHERE m.chatRoom = :chatRoom AND m.status = :status
+""")
+  List<ChatRoomMember> findByChatRoomAndStatus(
+      @Param("chatRoom") ChatRoom chatRoom,
+      @Param("status") ChatRoomMemberStatus status
+  );
+
+  @Query("SELECT c FROM ChatRoomMember c JOIN FETCH c.member WHERE c.chatRoom = :chatRoom AND c.member = :member")
+  Optional<ChatRoomMember> findByChatRoomAndMember(@Param("chatRoom") ChatRoom chatRoom, @Param("member") Member member);
+
+
+
+
+}
