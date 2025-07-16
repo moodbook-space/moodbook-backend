2854a3259bc73976dc052969265e0a85e171c575
roode <roode1017@gmail.com>
2025-07-16 14:06:47 +0900

    feat(serviceImpl) : 관리자 채팅방 serviceImpl 수정(#69)

--- src/main/java/org/com/moodbook/admin/chat/service/impl/AdminChatRoomServiceImpl.java
+++ src/main/java/org/com/moodbook/admin/chat/service/impl/AdminChatRoomServiceImpl.java
@@ -1,32 +1,36 @@
 package org.com.moodbook.admin.chat.service.impl;
 
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.com.moodbook.admin.chat.dto.AdminChatRoomDTO;
 import org.com.moodbook.admin.chat.repository.AdminChatRoomRepository;
 import org.com.moodbook.admin.chat.service.AdminChatRoomService;
+import org.com.moodbook.common.exception.BaseException;
+import org.com.moodbook.common.exception.ErrorCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class AdminChatRoomServiceImpl implements AdminChatRoomService {
 
   private final AdminChatRoomRepository adminChatRepository;
 
 
-
   //채팅방 리스트 페이지로 불러오기
+  @Transactional(readOnly = true)
   @Override
   public Page<AdminChatRoomDTO> getChatList(Pageable pageable) {
     return adminChatRepository.findAllPaging(pageable);
   }
 
   //채팅방 삭제
+  @Override
   public void deleteById(Long chatId){
     if(!adminChatRepository.existsById(chatId)){
-      throw new NoSuchElementException("해당 ID의 채팅방이 존재하지 않습니다.");
+      throw new BaseException(ErrorCode.CHATROOM_NOT_FOUND);
     }
     adminChatRepository.deleteById(chatId);
   }
