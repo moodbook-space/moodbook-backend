e0a22d8ebecaf05a3f55dc79fa9c5a02cd157e12
sagecode <iamsage9346@gmail.com>
2025-07-16 16:16:34 +0900

    feat/#85: BookEmotionScoreRepositoryCustom

--- src/main/java/org/com/moodbook/emotion/repository/BookEmotionScoreRepository.java
+++ src/main/java/org/com/moodbook/emotion/repository/BookEmotionScoreRepository.java
@@ -1,8 +1,11 @@
 package org.com.moodbook.emotion.repository;
 
+import java.util.List;
+import org.com.moodbook.common.constants.EmotionTag;
 import org.com.moodbook.emotion.entity.BookEmotionScore;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookEmotionScoreRepository extends MongoRepository<BookEmotionScore, Long> {
+public interface BookEmotionScoreRepository extends MongoRepository<BookEmotionScore, Long>, BookEmotionScoreRepositoryCustom {
+
 
 }
