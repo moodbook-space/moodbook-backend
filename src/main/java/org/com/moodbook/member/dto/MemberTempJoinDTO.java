b0a11ddb5493cb0fd4280dfbb5f84ac3430ad307
kimdonghyeon <dhb6306@gmail.com>
2025-07-16 10:37:31 +0900

    feat(DTO)- 임시회원가입 dto생성

--- src/main/java/org/com/moodbook/member/dto/MemberTempJoinDTO.java
+++ src/main/java/org/com/moodbook/member/dto/MemberTempJoinDTO.java
@@ -1,4 +1,30 @@
 package org.com.moodbook.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.com.moodbook.common.constants.Gender;
+import org.com.moodbook.common.constants.MemberStatus;
+import org.com.moodbook.common.constants.Role;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberTempJoinDTO {
+
+  private String email;
+  private String password;
+  private String name;
+  private Role role;
+  private String contact;
+  private boolean emailVerified;
+  private MemberStatus status;
+  private Gender gender;
+  private String address;
+  private String myImage;
+  private String nickname;
+
+
 }
