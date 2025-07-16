aa08a98d7a232390696ededa9210899b409a18a3
sagecode <iamsage9346@gmail.com>
2025-07-16 14:35:24 +0900

    feat/#79: securityConfig chat-rooms permitAll

--- src/main/java/org/com/moodbook/security/config/SecurityConfig.java
+++ src/main/java/org/com/moodbook/security/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                                
             .requestMatchers("/api/oauth/**").permitAll()
             .requestMatchers("/api/chat-rooms/**").permitAll()
+            .requestMatchers("/chat-rooms/**").permitAll()
             .requestMatchers(
                 "/api/oauth/",
                 "/admin/",
