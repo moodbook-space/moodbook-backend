7e3f305ba7b8a02251cc4eb1e4ff22ce499f1657
LteFroggy <zac0328@naver.com>
2025-07-17 14:28:15 +0900

    feat(entity) : 북마크 저장을 위한 Entity 추가 (#90)

--- src/main/java/org/com/moodbook/bookmark/entity/Bookmark.java
+++ src/main/java/org/com/moodbook/bookmark/entity/Bookmark.java
@@ -1,49 +1,52 @@
 package org.com.moodbook.bookmark.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.com.moodbook.book.entity.Book;
+import org.com.moodbook.common.model.BaseTime;
 import org.com.moodbook.member.entity.Member;
 
 @Entity
 @Getter
-@Builder
+@Table(name = "bookmark", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"member_id", "book_id"})})
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Bookmark {
+public class Bookmark extends BaseTime {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private boolean isDeleated = false;
-
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id", nullable = false)
   private Member member;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "book_id", nullable = false)
   private Book book;
 
-  // Factory Method. memberId, bookId기반으로 Entity를 생성하고 Bookmark에 넣는다.
+  // Factory Method. memberId, bookId기반으로 Bookmark Entity를 생성한다.
   public static Bookmark of(Long memberId, Long bookId) {
-    return Bookmark.builder()
-        .member(Member.builder()
-            .id(memberId).build())
-        .book(Book.builder()
-            .id(bookId).build())
-        .build();
+    return Bookmark.builder().member(Member.builder().id(memberId).build())
+        .book(Book.builder().id(bookId).build()).build();
+  }
+
+  // 팩토리 메소드 2. member와 book 객체를 받아 Bookmark Entity를 생성한다.
+  public static Bookmark of(Member member, Book book) {
+    return Bookmark.builder().member(member).book(book).build();
   }
 
 }
