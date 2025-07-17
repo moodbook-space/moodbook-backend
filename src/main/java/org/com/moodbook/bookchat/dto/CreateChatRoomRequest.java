44e81c1ad7c1e9852d07cae28e3ffa1e5868e54e
sagecode <iamsage9346@gmail.com>
2025-07-17 12:49:43 +0900

    chore/#100: CreateChatRoomRequest @setter 추가

--- src/main/java/org/com/moodbook/bookchat/dto/CreateChatRoomRequest.java
+++ src/main/java/org/com/moodbook/bookchat/dto/CreateChatRoomRequest.java
@@ -1,11 +1,14 @@
 package org.com.moodbook.bookchat.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class CreateChatRoomRequest {
   private String title;
   private int limitMembers;
+
+  @Setter
   private MemberDTO owner;
 
 }
