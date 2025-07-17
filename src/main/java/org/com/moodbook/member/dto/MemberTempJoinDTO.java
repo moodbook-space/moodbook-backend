2ad580b3954d9cff5591e59cc7e1d2617606d992
sagecode <iamsage9346@gmail.com>
2025-07-17 12:02:36 +0900

    chore/#96: enum type -> string type 변경

--- src/main/java/org/com/moodbook/member/dto/MemberTempJoinDTO.java
+++ src/main/java/org/com/moodbook/member/dto/MemberTempJoinDTO.java
@@ -17,11 +17,11 @@ public class MemberTempJoinDTO {
   private String email;
   private String password;
   private String name;
-  private Role role;
+  private String role;
   private String contact;
   private boolean emailVerified;
-  private MemberStatus status;
-  private Gender gender;
+  private String status;
+  private String gender;
   private String address;
   private String myImage;
   private String nickname;
