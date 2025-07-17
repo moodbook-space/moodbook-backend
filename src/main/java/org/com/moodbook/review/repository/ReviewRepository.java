e9f989ad1ecc45fad33799622c870cf7e2e1601b
codesche <codesche@gmail.com>
2025-07-17 11:07:34 +0900

    feat(repository): [Review] ReviewRepository 생성 (#76)

---
+++ src/main/java/org/com/moodbook/review/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package org.com.moodbook.review.repository;
+
+import org.com.moodbook.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(Long id, Pageable pageable);
+
+
+}
