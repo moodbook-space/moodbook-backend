c11dd05a7a80f599566eb3e01465bf113cbb94a4
codesche <codesche@gmail.com>
2025-07-17 11:09:37 +0900

    feat(repository): [RecentBookView] RecentBookViewRepository 생성 (#76)

---
+++ src/main/java/org/com/moodbook/recentbookviews/repository/RecentBookViewRepository.java
@@ -0,0 +1,17 @@
+package org.com.moodbook.recentbookviews.repository;
+
+import java.util.Optional;
+import org.com.moodbook.recentbookviews.entity.RecentBookView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecentBookViewRepository extends JpaRepository<RecentBookView, Long> {
+
+    Optional<RecentBookView> findByMemberIdAndBookId(Long userId, Long bookId);
+
+    Page<RecentBookView> findByMemberId(Long memberId, Pageable pageable);
+
+}
