289767b1f359051b1d7a7190bbf4e3cb22e3bd81
codesche <codesche@gmail.com>
2025-07-18 15:07:18 +0900

    chore(controller): [Like] Swagger 추가

--- src/main/java/org/com/moodbook/post/controller/LikeController.java
+++ src/main/java/org/com/moodbook/post/controller/LikeController.java
@@ -1,5 +1,9 @@
 package org.com.moodbook.post.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.com.moodbook.post.dto.LikeStatusResponse;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(name = "LikeController", description = "게시글 좋아요에 대한 컨트롤러")
 @RequiredArgsConstructor
 public class LikeController {
 
@@ -20,6 +25,12 @@ public class LikeController {
    * 좋아요 토글: 이미 좋아요가 있으면 취소, 없으면 생성
    */
   @PostMapping("/{postId}/like")
+  @Operation(summary = "좋아요 토글",
+      description = "이미 좋아요가 있으면 취소, 없으면 생성")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "좋아요가 생성되었습니다."),
+      @ApiResponse(responseCode = "500", description = "좋아요 생성에 완벽하게 실패했습니다.")
+  })
   public ResponseEntity<Void> toggleLike(
       @AuthenticationPrincipal CustomMemberDetails md,
       @PathVariable Long postId
@@ -32,6 +43,12 @@ public class LikeController {
    * 좋아요 개수와 내가 눌렀는지 여부 조회
    */
   @GetMapping("/{postId}/like")
+  @Operation(summary = "좋아요 개수와 내가 눌렀는지 여부 조회",
+      description = "좋아요 개수 및 자신이 좋아요를 눌렀는지의 여부 확인")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "정보를 정상적으로 조회하였습니다."),
+      @ApiResponse(responseCode = "500", description = "정보를 정상적으로 조회하는데 실패하였습니다.")
+  })
   public ResponseEntity<LikeStatusResponse> getLikeStatus(
       @AuthenticationPrincipal CustomMemberDetails md,
       @PathVariable Long postId
