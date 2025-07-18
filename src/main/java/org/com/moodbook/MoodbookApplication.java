28b556920fec689c0ded198e7c69eb67e865aaa0
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-18 15:06:38 +0900

    feat(MoodBookApplication): 엘라스틱서치 사용을 위한 어노테이션 추가(#119)

--- src/main/java/org/com/moodbook/MoodbookApplication.java
+++ src/main/java/org/com/moodbook/MoodbookApplication.java
@@ -2,16 +2,22 @@ package org.com.moodbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@EnableElasticsearchRepositories(basePackages = {
+    "org.com.moodbook.post.search",
+    "org.com.moodbook.book.search"
+})
 public class MoodbookApplication {
-    // dev 브랜치 생성
-    public static void main(String[] args) {
-        SpringApplication.run(MoodbookApplication.class, args);
-    }
+
+  // dev 브랜치 생성
+  public static void main(String[] args) {
+    SpringApplication.run(MoodbookApplication.class, args);
+  }
 
 }
