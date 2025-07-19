82d9251368594f6ad5ba28b78ead209db4283a79
LteFroggy <zac0328@naver.com>
2025-07-19 15:44:14 +0900

    chore(DTO) : 감정 저장을 MongoDB기반으로 수행하기 위한 DTO 수정 (#136)

--- src/main/java/org/com/moodbook/mypage/dto/MyPageResponse.java
+++ src/main/java/org/com/moodbook/mypage/dto/MyPageResponse.java
@@ -1,9 +1,7 @@
 package org.com.moodbook.mypage.dto;
 
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-import org.com.moodbook.common.constants.EmotionTag;
 import org.com.moodbook.member.entity.MemberProfile;
 
 @Getter
@@ -12,14 +10,10 @@ public class MyPageResponse {
 
   private String nickname;
   private String myImage;
-  private List<EmotionTag> emotions;
 
   // MemberProfile, Emotion의 List 받아서 MyPageResponse 만들기
-  public static MyPageResponse of(MemberProfile memberProfile, List<EmotionTag> emotions) {
-    return MyPageResponse.builder()
-        .nickname(memberProfile.getNickname())
-        .myImage(memberProfile.getMyImage())
-        .emotions(emotions)
-        .build();
+  public static MyPageResponse of(MemberProfile memberProfile) {
+    return MyPageResponse.builder().nickname(memberProfile.getNickname())
+        .myImage(memberProfile.getMyImage()).build();
   }
 }
