c38d080ec1ced32af5c891f5c1d4f8fbb739379c
codesche <codesche@gmail.com>
2025-07-20 15:28:26 +0900

    refactor(repository): [Review] JPA N + 1 문제 처리 (#140)

--- src/main/java/org/com/moodbook/review/repository/ReviewRepository.java
+++ src/main/java/org/com/moodbook/review/repository/ReviewRepository.java
@@ -3,13 +3,17 @@ package org.com.moodbook.review.repository;
 import org.com.moodbook.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    Page<Review> findByBookId(Long id, Pageable pageable);
+//    Page<Review> findByBookId(Long id, Pageable pageable);
 
+    /* N + 1 문제 처리 */
+    @EntityGraph(attributePaths = {"member", "member.memberProfile"})
+    Page<Review> findByBookId(Long bookId, Pageable pageable);
 
 }
