bef0520a495b68410ef0006a7096ae4a4acf9dbc
sagecode <iamsage9346@gmail.com>
2025-07-22 16:24:31 +0900

    feature/#160 : BookEsDocument - 책 조회 리스트에 필요한 dto

---
+++ src/main/java/org/com/moodbook/book/elasticsearch/dto/BookEsDocument.java
@@ -0,0 +1,42 @@
+package org.com.moodbook.book.elasticsearch.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.com.moodbook.book.dto.BookResponse;
+import org.com.moodbook.book.entity.Book;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Document(indexName = "book-index")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookEsDocument {
+
+  @Id
+  private Long bookId;
+
+  private String isbn13;
+  private String title;
+  private String author;
+  private BigDecimal reputation;
+  private String coverImage;
+
+  public static BookEsDocument fromEntity(Book dto) {
+    return BookEsDocument.builder()
+        .bookId(dto.getId())
+        .isbn13(dto.getIsbn13())
+        .title(dto.getTitle())
+        .author(dto.getAuthor())
+        .reputation(dto.getReputation())
+        .coverImage(dto.getCoverImage())
+        .build();
+  }
+}
