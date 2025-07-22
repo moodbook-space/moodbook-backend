111fe3e7579d5a9180b856e5ea97eed171eb8482
LteFroggy <zac0328@naver.com>
2025-07-22 15:14:17 +0900

    feat(service) : 마이페이지 내부에서 정보 수정 페이지를 제공하기 위한 service 수정 (#157)

--- src/main/java/org/com/moodbook/mypage/service/MyPageService.java
+++ src/main/java/org/com/moodbook/mypage/service/MyPageService.java
@@ -1,7 +1,8 @@
 package org.com.moodbook.mypage.service;
 
+import org.com.moodbook.mypage.dto.MyPageModifyRequest;
+import org.com.moodbook.mypage.dto.MyPageModifyResponse;
 import org.com.moodbook.mypage.dto.MyPageResponse;
-import org.com.moodbook.mypage.dto.UpdateNicknameDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MyPageService {
@@ -9,9 +10,12 @@ public interface MyPageService {
   // 마이페이지에 띄워야 할 기본정보(닉네임, 프로필사진, 기분리스트) 불러오기
   MyPageResponse getMyPageInfo(Long memberId);
 
-  // 내정보 업데이트하기, 현재는 닉네임만 업데이트할 수 있음.
-  MyPageResponse updateMyPageInfo(Long memberId, UpdateNicknameDTO updateNicknameDTO);
+  // 내정보 수정 페이지에 띄워야 할 정보 불러오기
+  MyPageModifyResponse getMyPageModifyInfo(Long memberId);
+
+  // 내정보 업데이트하기.
+  MyPageModifyResponse updateMyPageInfo(Long memberId, MyPageModifyRequest updateNicknameDTO);
 
   // 프로필사진 업데이트하기
-  MyPageResponse updateMyImage(Long memberId, MultipartFile image);
+  MyPageModifyResponse updateMyImage(Long memberId, MultipartFile image);
 }
