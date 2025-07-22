100803af4bbcfe0af3ccdd7c24a8a11c25fd10ad
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-07-22 19:04:52 +0900

    feat:(repository): 독서모임 repository 코드 추가(#144)

--- src/main/java/org/com/moodbook/post/repository/MeetingRepository.java
+++ src/main/java/org/com/moodbook/post/repository/MeetingRepository.java
@@ -1,13 +1,20 @@
 package org.com.moodbook.post.repository;
 
+import java.util.List;
 import org.com.moodbook.post.entity.Meeting;
 import org.com.moodbook.post.entity.Report;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
   Page<Meeting> findByMember_Id(Long memberId, Pageable pageable);
+
+  @Override
+  @EntityGraph(attributePaths = "moodTags")
+  List<Meeting> findAll();
+
 }
