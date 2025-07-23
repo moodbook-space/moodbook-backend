ac7a68f69f8790806254239503b452658cfa7ae6
kimdonghyeon <dhb6306@gmail.com>
2025-07-23 15:31:47 +0900

    feat(controller)-authenticationcontroller 비밀번호 재설정 api 설정

--- src/main/java/org/com/moodbook/security/authentication/controller/AuthenticationController.java
+++ src/main/java/org/com/moodbook/security/authentication/controller/AuthenticationController.java
@@ -1,11 +1,17 @@
 package org.com.moodbook.security.authentication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.com.moodbook.common.exception.BaseException;
+import org.com.moodbook.common.exception.ErrorCode;
 import org.com.moodbook.security.authentication.service.AuthenticationService;
 import org.com.moodbook.security.authentication.service.EmailAuthenticationService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +28,7 @@ import software.amazon.awssdk.services.s3.endpoints.internal.Value.Str;
 public class AuthenticationController {
   private final AuthenticationService authenticationService;
   private final EmailAuthenticationService emailAuthenticationService;
+  private final RedisTemplate<String, String> redisTemplate;
 
 
   //이메일 인증 api
@@ -45,4 +52,31 @@ public class AuthenticationController {
 
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
+  @Operation(summary = "비밀번호 재설정 인증",description = "입력한 메일로 비밀번호 재설정 인증링크 발송")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",description = "인증메일이 발송 되었습니다"),
+      @ApiResponse(responseCode = "404",description = "인증하려는 메일이 존재하지 않습니다")
+  })
+  @PostMapping("/reset-password/request")
+  public ResponseEntity<String> requestPasswordReset(@RequestParam String email) {
+    emailAuthenticationService.sendPasswordResetEmail(email);
+    return ResponseEntity.status(HttpStatus.OK).body("비밀번호 재설정 링크가 이메일로 전송되었습니다. \n메일함을 확인해주세요.");
+  }
+
+  @GetMapping("/reset-password")
+  public ResponseEntity<Void> redirectToPasswordReset(@RequestParam String token) {
+    Boolean isValid = redisTemplate.hasKey("password-reset:"+token);
+    if (isValid) {
+      //프론트의 재설정 페이지로 리다이렉트 ex:React 비밀번호 재설정 페이지
+      URI redirectUri = URI.create("http://localhost:8080/auth/password-reset?token="+token);
+      return ResponseEntity.status(HttpStatus.FOUND).location(redirectUri).build();//302리다이렉트
+    }else {
+      throw new BaseException(ErrorCode.INVALID_TOKEN);
+    }
+
+  }
+
+
+
+
 }
