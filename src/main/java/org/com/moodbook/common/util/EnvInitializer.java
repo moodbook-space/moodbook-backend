8674eb9672feee4a9e4cfa44c2e712cfc5f7938f
codesche <codesche@gmail.com>
2025-07-23 21:34:04 +0900

    perform: mail 관련 민감 내용 .env 에 적용 (#237)

---
+++ src/main/java/org/com/moodbook/common/util/EnvInitializer.java
@@ -0,0 +1,25 @@
+package org.com.moodbook.common.util;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvInitializer {
+
+    public static void init() {
+        Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+
+        setIfAbsent("MAIL_USERNAME", dotenv);
+        setIfAbsent("MAIL_PASSWORD", dotenv);
+    }
+
+    private static void setIfAbsent(String key, Dotenv dotenv) {
+        if (System.getenv(key) == null && System.getProperty(key) == null) {
+            String value = dotenv.get(key);
+            if (value != null) {
+                System.setProperty(key, value);
+            }
+        }
+    }
+
+}
