027f47b828a637369c7e10aae8d0924c96226d04
LteFroggy <zac0328@naver.com>
2025-07-24 15:36:11 +0900

    chore(dto) : BookController의 매핑을 바꾸면서 발생한 문제 해결 (#258)

--- src/main/java/org/com/moodbook/book/dto/BookEmotionRecommendAllRequest.java
+++ src/main/java/org/com/moodbook/book/dto/BookEmotionRecommendAllRequest.java
@@ -1,13 +1,16 @@
 package org.com.moodbook.book.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Builder
 public class BookEmotionRecommendAllRequest {
+
   private String emotionTag;
   private int page;     // 0부터 시작
   private int size;     // 한 페이지 크기
-
 }
