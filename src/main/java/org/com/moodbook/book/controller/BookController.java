7d34e98c8eb18829387c192a3350d55f780a3f25
LteFroggy <zac0328@naver.com>
2025-07-24 15:36:04 +0900

    chore(controller) : BookController의 매핑을 바꾸면서 발생한 문제 해결 (#258)

--- src/main/java/org/com/moodbook/book/controller/BookController.java
+++ src/main/java/org/com/moodbook/book/controller/BookController.java
@@ -4,13 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.com.moodbook.book.dto.BookEmotionRecommendAllRequest;
 import org.com.moodbook.book.dto.BookEmotionRecommendAllResponse;
-import org.com.moodbook.book.dto.BookEmotionRecommendRequest;
 import org.com.moodbook.book.dto.BookEmotionRecommendResponse;
 import org.com.moodbook.book.dto.BookResponse;
 import org.com.moodbook.book.service.BookService;
@@ -23,8 +21,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,102 +32,82 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class BookController {
 
-    private final BookService bookService;
+  private final BookService bookService;
 
-    @GetMapping(value = "/recommendations/star")
-    @Operation(summary = "알라딘 평점순 - 높은 순으로",
-        description = "알라딘에서 추출한 데이터를 가지고 평점 기준으로 반환")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "도서 리스트 반환 성공"),
-        @ApiResponse(responseCode = "500", description = "도서 리스트 반환 오류")
-    })
-    public ResponseEntity<Page<BookResponse>> getRecommendedBooks(
-        @PageableDefault(size = 10, page = 0) Pageable pageable,
-        @AuthenticationPrincipal CustomMemberDetails memberDetails) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        Page<BookResponse> recommendedBooks = bookService.getRecommendedBooks(pageable, memberId);
-        return ResponseEntity.status(HttpStatus.OK).body(recommendedBooks);
-    }
+  @GetMapping(value = "/recommendations/star")
+  @Operation(summary = "알라딘 평점순 - 높은 순으로", description = "알라딘에서 추출한 데이터를 가지고 평점 기준으로 반환")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "도서 리스트 반환 성공"),
+      @ApiResponse(responseCode = "500", description = "도서 리스트 반환 오류")})
+  public ResponseEntity<Page<BookResponse>> getRecommendedBooks(
+      @PageableDefault(size = 10, page = 0) Pageable pageable,
+      @AuthenticationPrincipal CustomMemberDetails memberDetails) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+    Page<BookResponse> recommendedBooks = bookService.getRecommendedBooks(pageable, memberId);
+    return ResponseEntity.status(HttpStatus.OK).body(recommendedBooks);
+  }
 
-    @Operation(summary = "전체 도서 조회",
-        description = "출판일 기준으로 전체 도서를 조회합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "전체 도서 리스트 반환 성공"),
-        @ApiResponse(responseCode = "500", description = "전체 도서 리스트 반환 오류")
-    })
-    @GetMapping("")
-    public ResponseEntity<Page<BookResponse>> getBooks(
-        @PageableDefault(size = 100, page = 0) Pageable pageable,
-        @AuthenticationPrincipal CustomMemberDetails memberDetails
-    ) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        Page<BookResponse> books = bookService.getAllBooks(pageable, memberId);
-        return ResponseEntity.status(HttpStatus.OK).body(books);
-    }
+  @Operation(summary = "전체 도서 조회", description = "출판일 기준으로 전체 도서를 조회합니다.")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "전체 도서 리스트 반환 성공"),
+      @ApiResponse(responseCode = "500", description = "전체 도서 리스트 반환 오류")})
+  @GetMapping("")
+  public ResponseEntity<Page<BookResponse>> getBooks(
+      @PageableDefault(size = 100, page = 0) Pageable pageable,
+      @AuthenticationPrincipal CustomMemberDetails memberDetails) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+    Page<BookResponse> books = bookService.getAllBooks(pageable, memberId);
+    return ResponseEntity.status(HttpStatus.OK).body(books);
+  }
 
-    @GetMapping("/{bookId}")
-    @Operation(summary = "도서 상세 조회",
-        description = "특정 도서를 상세 조회합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "도서 상세 정보 조회 성공"),
-        @ApiResponse(responseCode = "500", description = "도서 상세 정보 조회 실패")
-    })
-    public ResponseEntity<BookResponse> getBookDetail(
-        @PathVariable Long bookId, @AuthenticationPrincipal CustomMemberDetails memberDetails) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        BookResponse book = bookService.getBookById(bookId, memberId);
-        return ResponseEntity.status(HttpStatus.OK).body(book);
-    }
+  @GetMapping("/{bookId}")
+  @Operation(summary = "도서 상세 조회", description = "특정 도서를 상세 조회합니다.")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "도서 상세 정보 조회 성공"),
+      @ApiResponse(responseCode = "500", description = "도서 상세 정보 조회 실패")})
+  public ResponseEntity<BookResponse> getBookDetail(@PathVariable Long bookId,
+      @AuthenticationPrincipal CustomMemberDetails memberDetails) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+    BookResponse book = bookService.getBookById(bookId, memberId);
+    return ResponseEntity.status(HttpStatus.OK).body(book);
+  }
 
-    @GetMapping("/trending")
-    @Operation(summary = "조회수별 도서 조회",
-        description = "조회수별로 도서 조회를 합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "조회수별 도서 조회 성공"),
-        @ApiResponse(responseCode = "500", description = "조회수별 도서 조회 실패")
-    })
-    public ResponseEntity<Page<BookResponse>> getTrendingBooks(
-        @PageableDefault(size = 20, page = 0)
-        Pageable pageable, @AuthenticationPrincipal CustomMemberDetails memberDetails) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        Page<BookResponse> trendingBooks = bookService.getTrendingBooks(pageable, memberId);
-        return ResponseEntity.status(HttpStatus.OK).body(trendingBooks);
-    }
+  @GetMapping("/trending")
+  @Operation(summary = "조회수별 도서 조회", description = "조회수별로 도서 조회를 합니다.")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "조회수별 도서 조회 성공"),
+      @ApiResponse(responseCode = "500", description = "조회수별 도서 조회 실패")})
+  public ResponseEntity<Page<BookResponse>> getTrendingBooks(
+      @PageableDefault(size = 20, page = 0) Pageable pageable,
+      @AuthenticationPrincipal CustomMemberDetails memberDetails) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+    Page<BookResponse> trendingBooks = bookService.getTrendingBooks(pageable, memberId);
+    return ResponseEntity.status(HttpStatus.OK).body(trendingBooks);
+  }
 
-    @GetMapping("/recommend/emotion/top10")
-    @Operation(summary = "감정별 인기 추천 도서 Top10",
-        description = "감정별 인기 추천 도서 조회를 합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "인기 추천 도서 정보 조회 성공"),
-        @ApiResponse(responseCode = "500", description = "인기 추천 도서 정보 조회 실패")
-    })
-    public ResponseEntity<List<BookEmotionRecommendResponse>> getBooksByEmotionTop10(
-        @RequestBody @Valid BookEmotionRecommendRequest request,
-        @AuthenticationPrincipal CustomMemberDetails memberDetails
-    ) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        List<BookEmotionRecommendResponse> result = bookService.getBooksByEmotionTop10(request, memberId);
-        return ResponseEntity.ok(result);
-    }
+  @GetMapping("/recommend/emotion/top10")
+  @Operation(summary = "감정별 인기 추천 도서 Top10", description = "감정별 인기 추천 도서 조회를 합니다.")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "인기 추천 도서 정보 조회 성공"),
+      @ApiResponse(responseCode = "500", description = "인기 추천 도서 정보 조회 실패")})
+  public ResponseEntity<List<BookEmotionRecommendResponse>> getBooksByEmotionTop10(
+      @RequestParam String emotion, @AuthenticationPrincipal CustomMemberDetails memberDetails) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+    List<BookEmotionRecommendResponse> result = bookService.getBooksByEmotionTop10(emotion,
+        memberId);
+    return ResponseEntity.ok(result);
+  }
 
-    @GetMapping("/recommend/emotion/all")
-    @Operation(summary = "모든 감정별 추천 도서",
-        description = "감정별 추천 도서를 모두 조회합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "감정별 추천 도서 모두 조회 성공"),
-        @ApiResponse(responseCode = "500", description = "감정별 추천 도서 모두 조회 실패")
-    })
-    public ResponseEntity<List<BookEmotionRecommendAllResponse>> getBooksByEmotionAll(
-        @RequestBody @Valid BookEmotionRecommendAllRequest request,
-        @AuthenticationPrincipal CustomMemberDetails memberDetails,
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size
-    ) {
-        Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
-        request.setPage(page);
-        request.setSize(size);
-        List<BookEmotionRecommendAllResponse> result = bookService.getBooksByEmotionDesc(request, memberId);
-        return ResponseEntity.ok(result);
-    }
+  @GetMapping("/recommend/emotion/all")
+  @Operation(summary = "모든 감정별 추천 도서", description = "감정별 추천 도서를 모두 조회합니다.")
+  @ApiResponses({@ApiResponse(responseCode = "200", description = "감정별 추천 도서 모두 조회 성공"),
+      @ApiResponse(responseCode = "500", description = "감정별 추천 도서 모두 조회 실패")})
+  public ResponseEntity<List<BookEmotionRecommendAllResponse>> getBooksByEmotionAll(
+      @RequestParam String emotion, @AuthenticationPrincipal CustomMemberDetails memberDetails,
+      @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    Long memberId = (memberDetails != null) ? memberDetails.getId() : null;
+
+    BookEmotionRecommendAllRequest request = BookEmotionRecommendAllRequest.builder()
+        .emotionTag(emotion).page(page).size(size).build();
+
+    List<BookEmotionRecommendAllResponse> result = bookService.getBooksByEmotionDesc(request,
+        memberId);
+    return ResponseEntity.ok(result);
+  }
 
 }
