6ab6b86967d2e39a6251096a1a017c5ca1cc0e5a
roode <roode1017@gmail.com>
2025-07-25 15:23:56 +0900

    feat(delete) : 불 필요한 파일들 제거 (#188)

--- src/main/java/org/com/moodbook/admin/member/repository/AdminMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.com.moodbook.admin.member.repository;
-
-import org.com.moodbook.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminMemberRepository extends JpaRepository<Member, Long> {
-
-}
