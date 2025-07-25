7383695af6c7ffad680f898d98ec92174433e526
roode <roode1017@gmail.com>
2025-07-25 15:34:39 +0900

    feat(repository) : 관리자 기능 병합(멤버) (#188)

--- src/main/java/org/com/moodbook/member/repository/MemberRepository.java
+++ src/main/java/org/com/moodbook/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package org.com.moodbook.member.repository;
 
+import java.util.List;
 import java.util.Optional;
+import org.com.moodbook.bookchat.entity.ChatRoom;
 import org.com.moodbook.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +17,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
   //가입된 회원 중 로그인을 위한 이메일 대조
   Optional<Member> findByEmail(String email);
+
+  @Query("SELECT r FROM Member r ORDER BY r.createdAt DESC")
+  List<Member> findAllOrderByCreatedAtDesc();
+
+  @Query("SELECT m FROM Member m JOIN m.memberProfile p " +
+  "WHERE m.name LIKE %:query% OR p.address LIKE %:query% OR m.email LIKE %:query% OR p.nickname LIKE %:query% " +
+      " ORDER BY m.createdAt DESC ")
+  List<Member>findAllByFeatures(String query);
 }
