d3bddc99feb9c384a56da59af3398859e1b647db
kimdonghyeon <dhb6306@gmail.com>
2025-07-26 11:13:03 +0900

    feat(controller)- 스웨거 주석 추가

--- src/main/java/org/com/moodbook/security/authentication/controller/AuthenticationController.java
+++ src/main/java/org/com/moodbook/security/authentication/controller/AuthenticationController.java
@@ -32,7 +32,13 @@ public class AuthenticationController {
   private final RedisTemplate<String, String> redisTemplate;
   private final AppUrlProperties appUrlProperties;
 
+
   //이메일 인증 api
+  @Operation(summary = "회원가입 이메일 인증",description = "회원가입 진행 시 이메일 인증을 요청하는 메서드")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",description = "회원가입이 완료 되었습니다 인증 후 진행해주세요"),
+      @ApiResponse(responseCode = "500",description = "알 수 없는 오류입니다")
+  })
   @GetMapping("/verify-email")
   public ResponseEntity<String> verifyEmail(@RequestParam String token) {
 
@@ -41,6 +47,11 @@ public class AuthenticationController {
   }
 
   //access토큰 만료 시 토큰 재발급 메서드
+  @Operation(summary = "액세스 토큰 재발급",description = "access토큰 만료 시 refresh토큰 검증 후 access토큰 재발급")
+  @ApiResponses({
+      @ApiResponse(responseCode = "201",description = "accerssToken 재발급 완료"),
+      @ApiResponse(responseCode = "403",description = "refesh토큰 만료")
+  })
   @PostMapping("/reissue")
   public ResponseEntity<?> reissueToken(@RequestHeader("Authorization") String refreshToken) {
 
@@ -53,7 +64,7 @@ public class AuthenticationController {
 
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
-  @Operation(summary = "비밀번호 재설정 인증",description = "입력한 메일로 비밀번호 재설정 인증링크 발송")
+  @Operation(summary = "비밀번호 재설정 요청",description = "입력한 메일로 비밀번호 재설정 인증링크 발송")
   @ApiResponses({
       @ApiResponse(responseCode = "200",description = "인증메일이 발송 되었습니다"),
       @ApiResponse(responseCode = "404",description = "인증하려는 메일이 존재하지 않습니다")
@@ -64,6 +75,12 @@ public class AuthenticationController {
     return ResponseEntity.status(HttpStatus.OK).body("비밀번호 재설정 링크가 이메일로 전송되었습니다. \n메일함을 확인해주세요.");
   }
 
+  @Operation(summary = "비밀번호 재설정 인증",description = "링크 클릭 시 redis를 통한 비밀번호 재설정 인증확인")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",description = "인증 완료"),
+      @ApiResponse(responseCode = "403",description = "인증 토큰 부적합")
+
+  })
   @GetMapping("/reset-password/verify-token")
   public ResponseEntity<Void> redirectToPasswordReset(@RequestParam String token) {
     Boolean isValid = redisTemplate.hasKey("password-reset:"+token);
@@ -77,7 +94,13 @@ public class AuthenticationController {
 
 
   }
+
   //비밀번호 재설정 완료 메서드
+  @Operation(summary = "비밀번호 재설정",description = "사용자가 비밀번호 재설정하여 디비로 요청")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",description = "비밀번호 재설정이 완료 되었습니다"),
+      @ApiResponse(responseCode = "403", description = "만료되었거나 잘못 된 토큰입니다")
+  })
   @PostMapping("/reset-password/confirm")
   public ResponseEntity<?> confirmPasswordReset(@RequestBody PasswordResetRequestDto dto) {
     emailAuthenticationService.resetPassword(dto.getNewPassword(), dto.getToken());
